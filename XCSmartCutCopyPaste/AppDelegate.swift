e5f89e4a485da40c44a6e409aa860e2436e9b263
Mike Retondo <mretondo@mac.com>
2018-08-16 12:46:50 -0500

    Initial Commit

---
+++ XCSmartCutCopyPaste/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  XCSmartCutCopyPaste
+//
+//  Created by Mike Retondo on 8/16/18.
+//  Copyright © 2018 Mike Retondo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
